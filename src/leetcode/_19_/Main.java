f45311d7d2b841a5a321b77d1d19cbf9fe3578ed
coderbean <haozhangbo@hotmail.com>
2019-03-07 19:29:53 +0800

    19. Remove Nth Node From End of List

--- src/leetcode/_19_/Main.java
+++ src/leetcode/_19_/Main.java
@@ -6,7 +6,7 @@ package leetcode._19_;
 public class Main {
     public static void main(String[] args) {
         ListNode node = new ListNode(1).setNext(new ListNode(2).setNext(new ListNode(3).setNext(new ListNode(4).setNext(new ListNode(5)))));
-        // ListNode node = new ListNode(1).setNext(new ListNode(2)); // test2
+        // ListNode node = new ListNode(1).setNext(new ListNode(2));
         Solution solution = new Solution();
         ListNode listNode = solution.removeNthFromEnd(node, 2);
         System.out.println(listNode.toString());

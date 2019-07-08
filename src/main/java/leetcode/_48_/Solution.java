11a7f599e902bf3be917000d5734b4c41846f14a
coderbean <371327964@qq.com>
2019-07-08 10:06:47 +0800

    48. Rotate ImageCopy for MarkdownCopy for Markdown

--- src/main/java/leetcode/_48_/Solution.java
+++ src/main/java/leetcode/_48_/Solution.java
@@ -2,8 +2,7 @@ package leetcode._48_;
 
 class Solution { // 解决这个问题的关键就是知道，图像旋转90度是可以通过先对称镜像，然后对角线镜像可以得到旋转结果
     public void rotate(int[][] matrix) {
-        // 首先按照水平反转
-
+        // 首先垂直镜像反转
         for (int j = 0; j < matrix.length / 2; j++) {
             for (int i = 0; i < matrix.length; i++) {
                 int tmp = matrix[j][i];

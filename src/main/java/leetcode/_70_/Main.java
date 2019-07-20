bed9899bad1e3387ac6c3fbc1fc6b8dc9e6eca5b
coderbean <371327964@qq.com>
2019-07-20 17:11:23 +0800

    0070. Climbing Stairs

---
+++ src/main/java/leetcode/_70_/Main.java
@@ -0,0 +1,16 @@
+package leetcode._70_;
+
+/**
+ * Created by zhangbo54 on 2019-03-04.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println( solution.climbStairs(0));
+        System.out.println( solution.climbStairs(2));
+        System.out.println( solution.climbStairs(3));
+        System.out.println( solution.climbStairs(1));
+        System.out.println( solution.climbStairs(111));
+    }
+}
+
---
+++ src/main/java/leetcode/_70_/Solution.java
@@ -0,0 +1,21 @@
+package leetcode._70_;
+
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        return this.climbStairs(dp, n);
+
+    }
+
+    private int climbStairs(int[] dp, int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        if (dp[n] > 0) {
+            return dp[n];
+        }
+        int num = climbStairs(dp, n - 1) + climbStairs(dp, n - 2);
+        dp[n] = num;
+        return num;
+    }
+}
